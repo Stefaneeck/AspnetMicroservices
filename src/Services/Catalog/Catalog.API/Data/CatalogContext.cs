239627416f194463fc340230cd65d7873e55fb3e
stefaneeck <stefan.eeckhoudt@gmail.com>
2021-12-12 20:28:34 +0100

    Repository pattern implementation.

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,31 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data
+{
+    //gets data from the mongodb
+    public class CatalogContext : ICatalogContext
+    {
+        //aspnetcore built-in DI (used for configuration)
+        public CatalogContext(IConfiguration configuration)
+        {
+            //gets connectionstring value from appsettings.json
+            var client = new MongoClient(configuration.GetValue<string> ("DatabaseSettings:ConnectionString"));
+
+            //if there is no db, it will create one for us
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+
+            //populate products
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+
+            //seed data
+            //CatalogContextSeed.SeedData(Products);
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Data/Repositories/IProductRepository.cs
@@ -0,0 +1,27 @@
+﻿using Catalog.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data.Repositories
+{
+    public interface IProductRepository
+    {
+        //task = async
+        Task<IEnumerable<Product>> GetProducts();
+
+        Task<Product> GetProduct(string id);
+
+        Task<IEnumerable<Product>> GetProductByName(string name);
+
+        Task<IEnumerable<Product>> GetProductByCategory(string categoryName);
+
+        Task CreateProduct(Product product);
+
+        Task<bool> UpdateProduct(Product product);
+
+        Task<bool> DeleteProduct(string id);
+
+    }
+}
