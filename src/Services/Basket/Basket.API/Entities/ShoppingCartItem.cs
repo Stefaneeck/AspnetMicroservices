966fa6db2e70d6e72d5c6f9a72d719d0dbd64151
stefaneeck <stefan.eeckhoudt@gmail.com>
2021-12-17 21:53:52 +0100

    Basket API, repository. Redis.

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCartItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Entities
+{
+    public class ShoppingCartItem
+    {
+        public int Quantity { get; set; }
+
+        public string Color { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string ProductId { get; set; }
+
+        //We consume an api to communicate between microservices, double data because otherwise we need to consume the api only for the product name.
+        public string ProductName { get; set; }
+    }
+}
