190bf28f4259128859ffa21f9eba0a27c7efe6ee
stefaneeck <stefan.eeckhoudt@gmail.com>
2022-01-01 21:38:47 +0100

    Ordering API

---
+++ src/Services/Ordering/Ordering.Domain/Common/EntityBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.Common
+{
+    //common features of entities
+    public abstract class EntityBase
+    {
+        //protected set in order to use in the derived classes
+        public int Id { get; protected set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string LastModifiedBy { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+    }
+}
