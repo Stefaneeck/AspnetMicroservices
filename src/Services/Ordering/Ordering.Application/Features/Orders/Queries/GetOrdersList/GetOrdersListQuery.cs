4b008294bf485bdcdb45105e99d2323c60f18595
stefaneeck <stefan.eeckhoudt@gmail.com>
2022-01-02 21:06:38 +0100

    GetOrdersListQuery

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListQuery.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrdersList
+{
+    //filter with UserName
+    //IRequest<ResponseType>: the response type of this request is placed between <>
+    //OrdersVm = DTO
+    //mediator will trigger handler class when the request is placed
+    public class GetOrdersListQuery : IRequest<List<OrdersVm>>
+    {
+        public GetOrdersListQuery(string userName)
+        {
+            UserName = userName;
+        }
+
+        public string UserName { get; set; }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using MediatR;
+using Ordering.Application.Contracts.Persistence;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrdersList
+{
+    //this class will be triggered by mediator when the request is sent
+    //IRequestHandler<TRequest, TResponse>: GetOrdersListQuery is the request, List<OrdersVm> is the response
+    //working with abstractions because we only focus on the business requirments, not the implementation details
+    public class GetOrdersListQueryHandler : IRequestHandler<GetOrdersListQuery, List<OrdersVm>>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+
+        public GetOrdersListQueryHandler(IOrderRepository orderRepository, IMapper mapper)
+        {
+            _orderRepository = orderRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<OrdersVm>> Handle(GetOrdersListQuery request, CancellationToken cancellationToken)
+        {
+            var orderList = await _orderRepository.GetOrdersByUserName(request.UserName);
+            return _mapper.Map<List<OrdersVm>>(orderList);
+        }
+}
