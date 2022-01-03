b6a7367ee34cbc0e62343b76fa852da2f0ab66df
stefaneeck <stefan.eeckhoudt@gmail.com>
2022-01-03 22:40:02 +0100

    Pipeline behaviour

---
+++ src/Services/Ordering/Ordering.Application/Behaviours/UnhandledExceptionBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Behaviours
+{
+    public class UnhandledExceptionBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly ILogger<TRequest> _logger;
+
+        public UnhandledExceptionBehaviour(ILogger<TRequest> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            //this way we can handle all logging here, and not in every handler class method (for instance in CheckoutOrderCommandHandler) 
+            try
+            {
+                return await next();
+            }
+            catch (Exception ex)
+            {
+                var requestName = typeof(TRequest).Name;
+                _logger.LogError(ex, "Application request: Unhandled exception for request {name} {@Request}", requestName, request);
+
+                //re-throw exception
+                throw;
+            }
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Behaviours/ValidationBehaviour.cs
@@ -0,0 +1,78 @@
+﻿using FluentValidation;
+using MediatR;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+//make sure that we are using our own ValidationException class and not FluentValidations class
+using ValidationException = Ordering.Application.Exceptions.ValidationException;
+
+namespace Ordering.Application.Behaviours
+{
+    //this is our preprocessor class, every single mediator request has to reach this class
+    //In this class, we collect all the validations and run the validate methods before executing the request (good feature for performing validation middleware, by intercepting mediator requests) 
+    //implements mediator interface IPipelineBehaviour, we intercept before or after the handler class
+    public class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        //we collect all IValidator objects from the assembly (CheckoutOrderCommandValidator and UpdateOrderCommandValidator)
+        //we can do this because the validator classes extend AbstractValidator, which implements IValidator
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValidationBehaviour(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+
+        //next parameter: after we performed the validation behaviour, we can call the 'next' method in order to run an other behaviour
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            if(_validators.Any())
+            {
+                //you can perform some of the validations using FluentValidation context
+                var context = new ValidationContext<TRequest>(request);
+
+                //completes when all the tasks have been completed
+                //select all validators one by one and perform the ValidateAsync method
+                var validationResults = await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+
+                //check if any errors are not null
+                //the where clause refers to r.Errors, which is a list of ValidationFailure
+                //validationResults.Select(r => r.Errors) would return a list of list, selectmany flattens this
+
+                #region commentSelectMany
+                /* example:
+                 * 
+                 * public class PhoneNumber
+                {
+                    public string Number { get; set; }
+                }
+
+                public class Person
+                {
+                    public IEnumerable<PhoneNumber> PhoneNumbers { get; set; }
+                    public string Name { get; set; }
+                }
+
+                IEnumerable<Person> people = new List<Person>();
+
+                // Select gets a list of lists of phone numbers
+                IEnumerable<IEnumerable<PhoneNumber>> phoneLists = people.Select(p => p.PhoneNumbers);
+
+                // SelectMany flattens it to just a list of phone numbers.
+                IEnumerable<PhoneNumber> phoneNumbers = people.SelectMany(p => p.PhoneNumbers);
+                 */
+                #endregion
+
+                var validationFailures = validationResults.SelectMany(r => r.Errors).Where(f => f != null).ToList();
+
+                if(validationFailures.Count != 0)
+                {
+                    throw new ValidationException(validationFailures);
+                }
+            }
+
+            //after you handle your pipeline operations, you have to call the 'next' method in order to continue our requests, pipeline our requesthandler method in the mediator
+            return await next();
+        }
+    }
+}
