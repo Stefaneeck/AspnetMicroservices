e318038882039ce6d73faec1acff3595ccd81dd3
stefaneeck <stefan.eeckhoudt@gmail.com>
2022-01-03 23:07:09 +0100

    service registrations

--- src/Services/Ordering/Ordering.API/Startup.cs
+++ src/Services/Ordering/Ordering.API/Startup.cs
@@ -1,15 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Ordering.API
 {
@@ -25,6 +19,10 @@ namespace Ordering.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //extension methods
+            //it's good practice to handle every dependency in their own layer
+            //services.AddApplicationServices();
+            //services.AddInfrastructureServices();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
---
+++ src/Services/Ordering/Ordering.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Behaviours;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            //services used in application layer
+
+            //checks if there is any class that inherits from Profile, and executes the profiles when running the application
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            //checks if there is any class that inherits from the AbstractValidator
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            //checks if there is any class that inherits from IRequest or IRequestHandler
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            //define pipeline behaviours
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+
+            return services;
+        }
+    }
+}
