fd238e159432a388cdc8dcbd88ca293ea63dfa3a
stefaneeck <stefan.eeckhoudt@gmail.com>
2022-01-07 21:06:42 +0100

    applying ef core migrations automatically

---
+++ src/Services/Ordering/Ordering.API/Extensions/HostExtensions.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.API.Extensions
+{
+    public static class HostExtensions
+    {
+        public static IHost MigrateDatabase<TContext>(this IHost host,
+                                            Action<TContext, IServiceProvider> seeder,
+                                            int? retry = 0) where TContext : DbContext
+        {
+            //Value because nullable type
+            int retryForAvailability = retry.Value;
+
+            //This will provide the logger and the context object
+            //we can access these trough the var scope
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var logger = services.GetRequiredService<ILogger<TContext>>();
+                var context = services.GetService<TContext>();
+
+                try
+                {
+                    logger.LogInformation("Migrating database with context {DbContextName}", typeof(TContext).Name);
+
+                    InvokeSeeder(seeder, context, services);
+
+                    //if the sql server container is not created on run docker compose this
+                    //migration can't fail for network related exception. The retry options for DbContext only 
+                    //apply to transient exceptions                    
+                    //retry.Execute(() => InvokeSeeder(seeder, context, services));
+
+                    logger.LogInformation("Migrated database with context {DbContextName}", typeof(TContext).Name);
+
+                }
+                catch (SqlException ex)
+                {
+
+                    logger.LogError(ex, "An error occurred while migrating the database used on context {DbContextName}", typeof(TContext).Name);
+
+                    //retry because we are dependent on other containers which may not be ready yet
+                    if(retryForAvailability < 50)
+                    {
+                        retryForAvailability++;
+                        System.Threading.Thread.Sleep(2000);
+                        MigrateDatabase<TContext>(host, seeder, retryForAvailability);
+                    }
+                }
+
+            }
+            //needs to be returned because it's an extension method (chaining)
+            return host;
+        }
+
+        private static void InvokeSeeder<TContext>(Action<TContext, IServiceProvider> seeder,
+                                                    TContext context,
+                                                    IServiceProvider services) where TContext : DbContext
+        {
+            //will create the db if needed and run the migrations
+            context.Database.Migrate();
+            //seed db
+            //Action type (method as parameter), we dont know the generic type in this extension class
+            seeder(context, services);
+        }
+    }
+}
--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,11 +1,9 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.API.Extensions;
+using Ordering.Infrastructure.Persistence;
 
 namespace Ordering.API
 {
@@ -13,7 +11,38 @@ namespace Ordering.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            //CreateHostBuilder(args).Build().Run();
+
+            //because the containers should create their own dbs and data, we apply migrations automatically when ordering api starts up
+            var host = CreateHostBuilder(args).Build();
+            //our extension method
+            //first parameter is an action, you can think of this as passing a method as parameter
+            //migratedatabase does not have any knowledge about the TContext
+            //Our SeedAsync method in OrderContext is expecting the OrderContext type
+            //so thats way, to provide this context, we have to pass this seeding operation after the migration operation while providing the other context into the host method
+            //expects an Action and an int (first parameter IHost can be ignored because it is the extension method 'this' parameter
+            host.MigrateDatabase<OrderContext>((context, services) =>
+            {
+                var logger = services.GetService<ILogger<OrderContextSeed>>();
+                OrderContextSeed.SeedAsync(context, logger).Wait();
+            }
+            );
+            host.Run();
+
+            /* this code would do the same as the code above:
+             * 
+             * CreateHostBuilder(args)
+                .Build()
+                .MigrateDatabase<OrderContext>((context, services) =>
+                    {
+                        var logger = services.GetService<ILogger<OrderContextSeed>>();
+                        OrderContextSeed
+                            .SeedAsync(context, logger)
+                            .Wait();
+                    })
+                .Run();
+             * 
+             */
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
