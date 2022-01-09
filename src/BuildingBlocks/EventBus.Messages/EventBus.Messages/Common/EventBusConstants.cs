c1bdfb90330dc594dc4500a85329b6b7095caf5b
stefaneeck <stefan.eeckhoudt@gmail.com>
2022-01-09 20:34:27 +0100

    rabbit mq

---
+++ src/BuildingBlocks/EventBus.Messages/EventBus.Messages/Common/EventBusConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Messages.Common
+{
+    public static class EventBusConstants
+    {
+        //this is constant and should not change like a config value, that's why we set it here and not in appsettings.json for instance
+        //this queue name will be shown in rabbitmq management system in our docker image
+        public const string BasketCheckoutQueue = "basketcheckout-queue";
+    }
+}
