3e5ccf60e3b49d050d7946ef72062f3dc50b7ee4
stefaneeck <stefan.eeckhoudt@gmail.com>
2022-01-14 23:53:14 +0100

    product page

--- src/WebApps/AspnetRunBasics/Pages/Index.cshtml.cs
+++ src/WebApps/AspnetRunBasics/Pages/Index.cshtml.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
-using AspnetRunBasics.Repositories;
+using AspnetRunBasics.Models;
+using AspnetRunBasics.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,29 +9,48 @@ namespace AspnetRunBasics.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly IProductRepository _productRepository;
-        private readonly ICartRepository _cartRepository;
+        private readonly ICatalogService _catalogService;
+        private readonly IBasketService _basketService;
 
-        public IndexModel(IProductRepository productRepository, ICartRepository cartRepository)
+        public IndexModel(ICatalogService catalogService, IBasketService basketService)
         {
-            _productRepository = productRepository ?? throw new ArgumentNullException(nameof(productRepository));
-            _cartRepository = cartRepository ?? throw new ArgumentNullException(nameof(cartRepository));
+            _catalogService = catalogService;
+            _basketService = basketService;
         }
 
-        public IEnumerable<Entities.Product> ProductList { get; set; } = new List<Entities.Product>();
+        public IEnumerable<CatalogModel> ProductList { get; set; } = new List<CatalogModel>();
 
         public async Task<IActionResult> OnGetAsync()
         {
-            ProductList = await _productRepository.GetProducts();
+            ProductList = await _catalogService.GetCatalog();
             return Page();
         }
 
-        public async Task<IActionResult> OnPostAddToCartAsync(int productId)
+        public async Task<IActionResult> OnPostAddToCartAsync(string productId)
         {
-            //if (!User.Identity.IsAuthenticated)
-            //    return RedirectToPage("./Account/Login", new { area = "Identity" });
+            //get product details for the product that has been added to cart
+            //we get this from catalog microservice via ocelot gw
+            var product = await _catalogService.GetCatalog(productId);
 
-            await _cartRepository.AddItem("test", productId);
+            //set username, better would be to get this dynamically but this is not in the scope of this project
+            var userName = "swn";
+            
+            //get the 'old' basket for the given username
+            var basket = await _basketService.GetBasket(userName);
+
+            //add item to basket
+            basket.Items.Add(new BasketItemModel
+            {
+                ProductId = productId,
+                ProductName = product.Name,
+                Price = product.Price,
+                Quantity = 1,
+                Color = "Black"
+            });
+
+            //save changes trough call to basket microservice via ocelot
+            var basketUpdated = await _basketService.UpdateBasket(basket);
+
             return RedirectToPage("Cart");
         }
     }
